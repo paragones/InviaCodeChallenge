71c3db9d10c379339939320759b91925e0cba2ff
Paul Aragones <paragones@gmail.com>
2018-06-18 22:06:58 +0200

    Housekeeping

--- app/src/main/kotlin/com/inviacodechallenge/parag/modules/NetworkModule.kt
+++ app/src/main/kotlin/com/inviacodechallenge/parag/modules/NetworkModule.kt
@@ -43,95 +43,11 @@ class NetworkModule {
 
     private val client: OkHttpClient by lazy {
         OkHttpClient().newBuilder()
-//                .addInterceptor(bytesInterceptor)
                 .addInterceptor(HttpLoggingInterceptor().apply {
                     level = HttpLoggingInterceptor.Level.BODY
                 })
                 .build()
     }
-
-//    private val bytesInterceptor: Interceptor = Interceptor { chain ->
-//        val originalRequest = chain.request()
-//        val originalRequestBody = originalRequest.body()
-
-//        try {
-//            val copy = originalRequest.newBuilder().build()
-//            val buffer = okio.Buffer()
-//            copy.body()?.writeTo(buffer)
-//            Log.e(this.javaClass.simpleName, "buffer.readUtf8() -> ${buffer.readUtf8()}")
-//        } catch (e: IOException) {
-//            Log.e(this.javaClass.simpleName, "did not work")
-//        }
-
-
-//        val buffer = okio.Buffer()
-//        originalRequestBody?.writeTo(buffer)
-//        val fixedLengthRequestBody = RequestBody.create(originalRequestBody?.contentType(),buffer.snapshot())
-//        val newContent = ByteArray(1024)
-//        try {
-//            val newRequest = originalRequest.newBuilder()
-//                    .method()
-//                    .header("Content-Length", newContent.size.toString())
-////                    .method(originalRequest.method(), fixedLengthRequestBody)
-//                    .build()
-//
-//            Log.e(this.javaClass.simpleName, "newRequest.headers() : ${newRequest.headers()}")
-//            Log.e(this.javaClass.simpleName, "newRequest.method() : ${newRequest.method()}")
-//            Log.e(this.javaClass.simpleName, "fixedLengthRequestBody body : ${fixedLengthRequestBody.toString()}")
-
-//        } catch (e: Exception) {
-//            Log.e(this.javaClass.simpleName, "error : $e")
-//        }
-//        chain.proceed(originalRequest)
-//        chain.proceed(newRequest)
-//    }
-
-    private val bytesInterceptor: Interceptor = Interceptor { chain ->
-        val request = chain.request()
-//        Log.e(this.javaClass.simpleName, "call ==> " + request.url())
-        val response = chain.proceed(request)
-        val responseBody = response?.body()
-        val source = responseBody?.source()
-        source?.request(java.lang.Long.MAX_VALUE) // Buffer the entire body.
-        val buffer = source?.buffer()
-        val newResponse = response.newBuilder()
-                .body(ResponseBody.create(responseBody?.contentType(),buffer.toString()))
-                .build()
-
-
-        Log.e(this.javaClass.simpleName, "response? ${response.isSuccessful}")
-        Log.e(this.javaClass.simpleName, "response? ${response.message()}")
-        Log.e(this.javaClass.simpleName, "responseBody? ${responseBody?.contentType()}")
-        Log.e(this.javaClass.simpleName, "responseBody? ${responseBody?.contentLength()}")
-        Log.e(this.javaClass.simpleName, "responseBody? ${responseBody.toString()}")
-
-
-//        val requestBuffer = Buffer()
-//        response.body().(requestBuffer)
-//        Log.d("OkHttp", requestBuffer.readUtf8())
-
-        newResponse
-//        return@Interceptor newResponse
-    }
-
-//    class ResponseInterceptor : Interceptor {
-//
-//        @Throws(IOException::class)
-//        override fun intercept(chain: Interceptor.Chain): Response {
-//            val request = chain.request()
-//            Log.e(this.javaClass.simpleName, "call ==> " + request.url())
-//            val response = chain.proceed(request)
-//            val responseBody = response?.body()
-//            val source = responseBody?.source()
-//            source?.request(java.lang.Long.MAX_VALUE) // Buffer the entire body.
-//            val buffer = source?.buffer()
-//            val newResponse = response.newBuilder()
-//                    .body(ResponseBody.create(responseBody?.contentType(),buffer.toString()))
-//                    .build()
-//            return newResponse
-//        }
-//
-//    }
 
     private fun defaultGson(): Gson {
         return GsonBuilder()
@@ -143,6 +59,5 @@ class NetworkModule {
         @Throws(JsonParseException::class)
         override fun deserialize(src: JsonElement, srcType: Type,
                                  context: JsonDeserializationContext): Uri = Uri.parse(src.asString)
-//                                 context: JsonDeserializationContext): Uri = Uri.parse(String.format("http:%s",src.asString))
     }
 }
